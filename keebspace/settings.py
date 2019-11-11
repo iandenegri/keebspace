dfa1eab6cbadc810d8d1e1172f3f7f9bf4eba30c
iandenegri <iandenegri@hotmail.com>
2019-11-10 20:24:06 -0500

    Fix login and logout redirects

--- keebspace/settings.py
+++ keebspace/settings.py
@@ -117,8 +117,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL='users.CustomUser'
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
 
 ###################################################
 ### Internationalization

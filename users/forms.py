4d0236e8fdbae79a19bffb5d67a1047204289e69
iandenegri <iandenegri@hotmail.com>
2019-11-10 21:52:30 -0500

    Support for birthday. Better Navbar.

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,17 @@
 from django import forms
+from django.forms.widgets import SelectDateWidget
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-
+    # Not needed here but is a nice snippet I'll be using later.
+    # date_of_birth = forms.DateField(
+    #     label="Date of Birth",    
+    #     widget=SelectDateWidget(
+    #         empty_label=("Choose Year", "Choose Month", "Choose Day"),
+    #     ),
+    # )
     class Meta:
         model = CustomUser
         fields = ('username', 'email')
